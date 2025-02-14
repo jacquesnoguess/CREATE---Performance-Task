b8190d71f96cdbc451ddc7ed0074c593716e220b
jacquesnoguess <longstrangetrip77@gmail.com>
2025-02-14 16:30:24 -0600

    testing

---
+++ main.py
@@ -0,0 +1,31 @@
+balances = {
+    'John':100,
+    'Bob':50
+}
+
+def deposit():
+    pass
+
+def withdraw():
+    print('dawg')
+
+def check_balances():
+    pass
+
+def main():
+    while True:
+        name = input('Welcome. Enter your account name: ')
+        if name in balances:
+            action = input("Welcome back "+name+'. What would you like to do? \n [Withdraw]    [Deposit]    [Check Balance] \n\n')
+            if action.lower == 'withdraw':
+                print('yeah')
+            elif action.lower == 'deposit':
+                print('awesome')
+            elif action.lower == 'check balance':
+                print('ah')
+            else:
+                input('Action not recognized. Try again?')
+        else:
+            print('no')
+
+main()
