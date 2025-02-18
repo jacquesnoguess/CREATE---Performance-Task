4f4ac5dfbbeb35a5d18fccce5dd283f99a05be1e
jacquesnoguess <longstrangetrip77@gmail.com>
2025-02-18 15:54:57 -0600

    basically done

--- main.py
+++ main.py
@@ -1,31 +1,74 @@
 balances = {
-    'John':100,
-    'Bob':50
+    'john':100,
+    'bob':50
 }
 
-def deposit():
-    pass
+def await_action1(name):
+    action = input("Welcome back "+name+'. What would you like to do? \n [Withdraw]    [Deposit]    [Check Balance]    [Exit] \n\n')
+    if action.lower() == 'withdraw':
+        withdraw(balances, name)
+    elif action.lower() == 'deposit':
+        deposit(balances, name)
+    elif action.lower() == 'check balance':
+        check_balances(balances,name)
+    elif action.lower() == 'exit' or 'quit':
+        quit()
+    else:
+        input('Action not recognized. Try again?')
 
-def withdraw():
-    print('dawg')
+def await_action2(name):
+    action = input("\nWhat would you like to do? \n [Withdraw]    [Deposit]    [Check Balance]    [Exit] \n\n")
+    if action.lower() == 'withdraw':
+        withdraw(balances, name)
+    elif action.lower() == 'deposit':
+        deposit(balances, name)
+    elif action.lower() == 'check balance':
+        check_balances(balances,name)
+    elif action.lower() == 'exit' or 'quit':
+        quit()
+    else:
+        input('Action not recognized. Try again?')
 
-def check_balances():
-    pass
+def create_account(balances):
+    name = input("What would you like your name to be: \n")
+    balances.update({name:0})
+
+def deposit(balances, name):
+    print("\nYour current balance is "+str(balances[name])+"$")
+    amount = int(input('How much would you like to deposit? \n\n'))
+    newbalance = balances[name]+amount
+    print('\nNew balance: '+str(int(newbalance))+"$")
+    balances.update({name:newbalance})    
+    await_action2(name)
+
+def withdraw(balances, name):
+    print("\nYour current balance is "+str(balances[name])+"$")
+    amount = int(input('How much would you like to withdraw? \n\n'))
+    if amount > balances[name]:
+        print("\nError: this account doesn't have that much money. Please try again.")
+        withdraw(balances,name)
+    else:
+        newbalance = balances[name]-amount
+        balances.update({name:newbalance})
+        print('\nNew balance: '+str(int(newbalance))+"$")
+        await_action2(name)
+
+
+def check_balances(balances,name):
+    print("\nYour balance is "+str(balances[name])+"$")
+    await_action2(name)
 
 def main():
     while True:
         name = input('Welcome. Enter your account name: ')
-        if name in balances:
-            action = input("Welcome back "+name+'. What would you like to do? \n [Withdraw]    [Deposit]    [Check Balance] \n\n')
-            if action.lower() == 'withdraw':
-                print('yeah')
-            elif action.lower() == 'deposit':
-                print('awesome')
-            elif action.lower() == 'check balance':
-                print('ah')
-            else:
-                input('Action not recognized. Try again?')
+        if name.lower() in balances:
+            await_action1(name)
         else:
-            print('no')
+            yn = input('Name not found in database. Would you like to create an account?\n[Yes]    [No]\n')
+            if yn.lower() == 'yes' or 'y':
+                create_account(balances)
+            if yn.lower() == 'no' or 'n':
+                main()
+
 
 main()
